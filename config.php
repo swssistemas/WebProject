e73d02ee3a84cb29e9f2e423fa25379c60e3b93e
Fabrício César <fabricio@sws.inf.br>
2017-01-26 08:38:17 -0200

    webProject

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencedison/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencedison/');
+
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp1\htdocs\opencedison/catalog/');
+define('DIR_SYSTEM', 'C:\xampp1\htdocs\opencedison/system/');
+define('DIR_LANGUAGE', 'C:\xampp1\htdocs\opencedison/catalog/language/');
+define('DIR_TEMPLATE', 'C:\xampp1\htdocs\opencedison/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\xampp1\htdocs\opencedison/system/config/');
+define('DIR_IMAGE', 'C:\xampp1\htdocs\opencedison/image/');
+define('DIR_CACHE', 'C:\xampp1\htdocs\opencedison/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp1\htdocs\opencedison/system/download/');
+define('DIR_UPLOAD', 'C:\xampp1\htdocs\opencedison/system/upload/');
+define('DIR_MODIFICATION', 'C:\xampp1\htdocs\opencedison/system/modification/');
+define('DIR_LOGS', 'C:\xampp1\htdocs\opencedison/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencedison');
+define('DB_PREFIX', 'oc_');
