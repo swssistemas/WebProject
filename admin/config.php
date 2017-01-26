984badded029ad680b900698795244ff6d61ac7a
Fabrício César <fabricio@sws.inf.br>
2017-01-26 09:48:10 -0200

    webProject

--- admin/config.php
+++ admin/config.php
@@ -1,28 +1,27 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://localhost/opencedison/admin/');
-
+define('HTTP_SERVER', 'http://localhost/opencart/');
 // HTTPS
-define('HTTPS_SERVER', 'http://localhost/opencedison/admin/');
-
+define('HTTPS_SERVER', 'http://localhost/opencart/');
+$raiz = $_SERVER['DOCUMENT_ROOT']."/opencart";
 // DIR
-define('DIR_APPLICATION', 'C:\xampp1\htdocs\opencedison/admin/');
-define('DIR_SYSTEM', 'C:\xampp1\htdocs\opencedison/system/');
-define('DIR_LANGUAGE', 'C:\xampp1\htdocs\opencedison/admin/language/');
-define('DIR_TEMPLATE', 'C:\xampp1\htdocs\opencedison/admin/view/template/');
-define('DIR_CONFIG', 'C:\xampp1\htdocs\opencedison/system/config/');
-define('DIR_IMAGE', 'C:\xampp1\htdocs\opencedison/image/');
-define('DIR_CACHE', 'C:\xampp1\htdocs\opencedison/system/cache/');
-define('DIR_DOWNLOAD', 'C:\xampp1\htdocs\opencedison/system/download/');
-define('DIR_UPLOAD', 'C:\xampp1\htdocs\opencedison/system/upload/');
-define('DIR_LOGS', 'C:\xampp1\htdocs\opencedison/system/logs/');
-define('DIR_MODIFICATION', 'C:\xampp1\htdocs\opencedison/system/modification/');
-define('DIR_CATALOG', 'C:\xampp1\htdocs\opencedison/catalog/');
-
+define('DIR_APPLICATION', $raiz.'/');
+define('DIR_SYSTEM', $raiz.'/system/');
+define('DIR_IMAGE', $raiz.'/image/');
+define('DIR_LANGUAGE', $raiz.'/language/');
+define('DIR_TEMPLATE', $raiz.'/view/template/');
+define('DIR_CONFIG', $raiz.'/system/config/');
+define('DIR_CACHE', $raiz.'/system/storage/cache/');
+define('DIR_DOWNLOAD', $raiz.'/system/storage/download/');
+define('DIR_LOGS', $raiz.'/system/storage/logs/');
+define('DIR_MODIFICATION', $raiz.'/system/storage/modification/');
+define('DIR_UPLOAD', $raiz.'/system/storage/upload/');
+define('DIR_CATALOG', $raiz.'/catalog/');
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_DATABASE', 'opencedison');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
